7f63e01cb088b12ab30edf60cf46ed92ca4a7be9
brandonsladek <brandonsladek@gmail.com>
2016-01-19 19:30:07 -0700

    Added names to top of bin_packing.py

---
+++ bin_packing.py
@@ -0,0 +1,22 @@
+import driver
+
+# Brandon Sladek and Alex Calderwood
+
+"""
+FIND_SOLUTION:
+    Define this function in BinPacking.py, along with any auxiliary
+functions that you need.  Do not change the Driver.py file at all.
+--------------------------------------------------
+rectangles: a list of tuples, e.g. [(w1, l1), ... (wn, ln)] where
+    w1 = width of rectangle 1,
+    l1 = length of rectangle 1, etc.
+--------------------------------------------------
+RETURNS: a list of tuples that designate the top left corner placement,
+         e.g. [(x1, y1), ... (xn, yn)] where
+         x1 = top left x coordinate of rectangle 1 placement
+         y1 = top left y coordinate of rectangle 1 placement, etc.
+"""
+
+def find_solution(rectangles):
+    return driver.find_naive_solution(rectangles)  # a working example!
+
